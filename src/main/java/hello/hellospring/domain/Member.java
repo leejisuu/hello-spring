9be566cdf4f38c9922debdd446f57e447293e224
leejisuu <dlwltn966@gmail.com>
2022-03-17 17:58:45 +0900

    인프런 스프링 입문 : 섹션 6 - 5 수강중

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,14 +1,16 @@
 package hello.hellospring.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+// ORM
+// @Entity 태그를 달면 JPA에서 관리하는 엔티티 설정
 @Entity
 public class Member {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)// PK 설정. identity 전략 => DB가 알아서 시퀀스 생성
     private Long id; // 시스템이 저장하는 아이디
+
     private String name;
 
     public Long getId() {
