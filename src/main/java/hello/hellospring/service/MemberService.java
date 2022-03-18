b45ccc91fa69ef08973f3ab0e0f4462af1666430
leejisuu <dlwltn966@gmail.com>
2022-03-18 16:39:41 +0900

    인프런 스프링 입문 : 섹션 6 완강

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,12 +4,15 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 // bean 등록을 해주지 않으면 컨테이너에서 인식 못함
 // @Service
+
+@Transactional
 public class MemberService { // ctrl + shitf + T -> Test 생성 가능
 
     private final MemberRepository memberRepository;
@@ -23,7 +26,7 @@ public class MemberService { // ctrl + shitf + T -> Test 생성 가능
     /*
     *  회원 가입
     * */
-    public Long join(Member member) {
+    public Long join(Member member) { // jpa는 모든 join은 트랜잭션 안에서 수행되어야 함
         // 같은 이름이 있는 중복 회원 X
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
