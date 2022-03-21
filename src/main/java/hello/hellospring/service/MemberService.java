1feae8011d14df3e78a613728f1664e41b81ee6a
leejisuu <dlwltn966@gmail.com>
2022-03-21 12:32:43 +0900

    인프런 스프링 입문 : 수강 완료

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -28,7 +28,12 @@ public class MemberService { // ctrl + shitf + T -> Test 생성 가능
     * */
     public Long join(Member member) { // jpa는 모든 join은 트랜잭션 안에서 수행되어야 함
 
-        long start = System.currentTimeMillis();
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+
+        // 시간을 찍던 로직을 AOP로 따로 정의
+        /*long start = System.currentTimeMillis();
 
         try {
             // 같은 이름이 있는 중복 회원 X
@@ -39,7 +44,7 @@ public class MemberService { // ctrl + shitf + T -> Test 생성 가능
             long finish = System.currentTimeMillis();
             long timeMs = finish - start;
             System.out.println("join = " + timeMs + "ms");
-        }
+        }*/
     }
 
     private void validateDuplicateMember(Member member) {
@@ -53,17 +58,7 @@ public class MemberService { // ctrl + shitf + T -> Test 생성 가능
     * 전체 회원 조회
    * */
     public List<Member> findMembers() {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
-
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
